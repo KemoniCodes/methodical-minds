b8bca806bb2bcc2e9f56bfcf3c696af321866a78
Kemoni <58960485+KemoniCodes@users.noreply.github.com>
2020-04-21 21:10:16 -0700

    created auth routes

--- application.py
+++ application.py
@@ -26,3 +26,11 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
